297b085c053ce230fe4789c6b199638ed0631a24
israt-jahan116 <iisrat1570@gmail.com>
2026-07-06 19:39:26 +0800

    Initial commit - Array Traversal

---
+++ main.c
@@ -0,0 +1,11 @@
+//Question:Write a C programe to traverse and print the elements of an array.
+#include<stdio.h>
+int main()
+{
+  int LA[]={10,20,30,40,50};
+  for(int i=0;i<5;i++)
+   {
+       printf("%d ",LA[i]);
+   }
+   return 0;
+}
