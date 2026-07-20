8321e3d15c4e3eeadae3f757009811a87bebb6c4
israt-jahan116 <iisrat1570@gmail.com>
2026-07-20 23:18:22 +0800

    linear +insert

---
+++ linear search+insert.c
@@ -0,0 +1,50 @@
+ #include<stdio.h>
+int main()
+{
+ int LA[20];
+ int n;
+ int target;
+ int value,k;
+ int i,j;
+ int count=0;
+ scanf("%d",&n);
+ for(i=0;i<n;i++)
+   {
+     scanf("%d",&LA[i]);
+
+   }
+
+   scanf("%d",&target);
+   for(i=0;i<n;i++)
+    {
+        if(LA[i]==target)
+        {
+            printf("Found:%d",LA[i]);
+            count++;
+            break;
+
+        }
+    }
+
+   if(count==0)
+   {
+       printf("Not found");
+       scanf("%d%d",&k,&value);
+       j=n-1;
+       while(j>=k)
+       {
+           LA[j+1]=LA[j];
+           j--;
+       }
+       LA[k]=value;
+       n++;
+       printf("After insert:");
+       for(i=0;i<n;i++)
+       {
+           printf("%d",LA[i]);
+       }
+
+   }
+   return 0;
+
+}
